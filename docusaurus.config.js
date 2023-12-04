b66619dfc4a35697c43cad409b524f26384c0c5c
Ykedy <gitsony@163.com>
2023-12-04 11:50:11 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ const katex = require("rehype-katex");
 const config = {
   title: "TODO",
   tagline: "The Universal Credit Protocol",
-  url: "https://TODO",
-  baseUrl: "/",
+  url: "https://omnisciate.github.io/docs/",
+  baseUrl: "/docs/",
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
   favicon: "favicon.png",
